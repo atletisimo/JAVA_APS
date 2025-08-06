69e91bc7aa3f2d76db7e61e940e6105074ff011e
atletisimo <37110896+atletisimo@users.noreply.github.com>
2025-08-06 15:19:56 +0200

    Update SLLParenDuplikati.java

--- SLLParenDuplikati.java
+++ SLLParenDuplikati.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class SLLParenDuplikati {
 	public static void main(String [] args) {
 		Scanner input=new Scanner(System.in);
-		int n=input.nextInt();//11
+		int n=input.nextInt();// Број на елементи, пример: 11
 		SLL<Integer> lista=new SLL<Integer>();
 	
 		for(int i=0;i<n;i++) {
@@ -11,17 +11,19 @@ public class SLLParenDuplikati {
 		}
 		SLLNode<Integer>jazol=lista.getFirst();
 		int counter=0;
-		int info=input.nextInt();
+		int info=input.nextInt();// Елементот што го проверуваме, пример: 2
 		SLLNode<Integer>lastOccurance=null;
+		// Проаѓање низ листата за броење на појави на info и чување на последната појава
 		while(jazol!=null) {
 			int el;
 			if(jazol.element==info) {
 				counter++;
-				lastOccurance=jazol;
+				lastOccurance=jazol;// Секојпат кога го најдеме, го ажурираме
 			}
 			
 			jazol=jazol.succ;
 		}
+				// Ако бројот на појави е НЕПАРЕН, ја бришеме последната појава
 		if(counter%2!=0&&lastOccurance!=null) {
 			lista.delete(lastOccurance);
 		}
