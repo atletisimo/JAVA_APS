9f7f164a25dfd995d53c62e7015ebb6f7146e2d2
atletisimo <37110896+atletisimo@users.noreply.github.com>
2025-08-07 13:03:26 +0200

    Update SLLPomestiNaKraj.java

--- SLLPomestiNaKraj.java
+++ SLLPomestiNaKraj.java
@@ -29,3 +29,48 @@ for(int i=0;i<n;i++) {
 	System.out.print(lista);
 }
 }
+
+/*
+
+package listi;
+
+import java.util.*;
+
+public class SLLPomestiNaKraj {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        input.nextLine(); // за чистење на новиот ред
+
+        SLL<String> lista = new SLL<>();
+
+        for (int i = 0; i < n; i++) {
+            lista.insertLast(input.nextLine());
+        }
+
+        int L = input.nextInt();
+
+        SLLNode<String> jazol = lista.getFirst();
+        int i = 0;
+
+        while (i < n && jazol != null) {
+            SLLNode<String> next = jazol.succ; // зачувај го следниот јазол
+
+            if (jazol.element.length() == L) {
+                String value = jazol.element; // зачувај го елементот
+                lista.delete(jazol);          // избриши го тековниот јазол
+                lista.insertLast(value);      // додади го на крај
+            }
+
+            jazol = next; // продолжи со следниот
+            i++;
+        }
+
+        System.out.println(lista);
+    }
+}
+
+
+
+
+*/
