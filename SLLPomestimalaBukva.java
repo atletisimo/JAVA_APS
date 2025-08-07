d9205406050b2813ef6c179ed5a2dd06f984bff8
atletisimo <37110896+atletisimo@users.noreply.github.com>
2025-08-07 16:22:40 +0200

    Update SLLPomestigolemaBukva.java

--- SLLPomestimalaBukva.java
+++ SLLPomestimalaBukva.java
@@ -19,15 +19,27 @@ public class SLLPomestimalaBukva {
         int count = 0;
 
         while (jazol != null && count < n) {
+            //count < n — итерираме само низ оригиналните n елементи, дури и ако додадеме нови на крај. Ова е многу важно за да не се вратиме на елементите што сме ги преместиле.
             SLLNode<String> next = jazol.succ;
             char firstChar = jazol.element.charAt(0);
 
             if (Character.isUpperCase(firstChar)) {
                 lista.insertLast(jazol.element);
-                if (prev == null) {
+                if (prev == null) {//Овој услов проверува дали тековниот јазол (jazol) што го гледаме е првиот елемент во листата.
                     lista.deleteFirst();
                 } else {
                     lista.delete(jazol);
+                    /*
+
+ко сакаме да го избришеме првиот јазол
+
+lista.delete(jazol) – ако сакаме да избришеме било кој друг јазол
+
+Но, ако се обидеш да повикаш lista.delete(jazol) за првиот јазол без да го знаеш претходниот, нема да знаеш како да го откачиш од листата.
+
+Затоа се користи if (prev == null) – да се детектира ако тековниот јазол е прв во листата, и тогаш да се повика deleteFirst() наместо обичното delete(jazol).
+
+                    */
                 }
             } else {
                 prev = jazol;
