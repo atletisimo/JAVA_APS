1de0e34cc15ad49e51adce941426e43de934d71c
atletisimo <37110896+atletisimo@users.noreply.github.com>
2025-08-29 21:50:41 +0200

    Update StackPonistuvanjeTopcinja.java

--- StackPonistuvanjeTopcinja.java
+++ StackPonistuvanjeTopcinja.java
@@ -1,79 +1,61 @@
-public class Topcinja {
-    public static String ponistiTopcinja(String[] vlez) {
-        ArrayStack<String> crveni = new ArrayStack<String>(100);
-        ArrayStack<String> zeleni = new ArrayStack<String>(100);
-        ArrayStack<String> sini = new ArrayStack<String>(100);
-
-        String izlez = "";
-        int n = 0;
-
-        for (int i = 0; i < vlez.length; i++) {
-            String s = vlez[i];
-
-            if (s.charAt(0) == 'R') {
-                if (!crveni.isEmpty()) {//Ако стекот е празен, crveni.peek() ќе фрли NoSuchElementException (или слична грешка), бидејќи нема елемент на врвот,затоа 
-                  //мора да се направи оваа проверка,//bidejki ne mozeme peek() da pravime ako stack-ot e prazen ke frli NoSuchElementException bidejki nema element na vrvot i zatoa mora ovaa proverka da se napravi
-                    if (crveni.peek().equals(s))
-                        crveni.push(s);
-                    else
-                        crveni.pop();
-                } else crveni.push(s);// Стекот е празен → немаме претходно топче од иста боја → нема што да се поништи → новото топче го додаваме во стекот.
-            }//Ако стекот не е празен, тогаш можеме да го споредиме врвното топче со новото:
-
-
-
-            if (s.charAt(0) == 'G') {
-                if (!zeleni.isEmpty()) {
-                    if (zeleni.peek().equals(s))
-                        zeleni.push(s);
-                    else
-                        zeleni.pop();
-                } else zeleni.push(s);
-            }
-
-            if (s.charAt(0) == 'B') {
-                if (!sini.isEmpty()) {
-                    if (sini.peek().equals(s))
-                        sini.push(s);
-                    else
-                        sini.pop();
-                } else sini.push(s);
-            }
-        }
-
-        while (!crveni.isEmpty()) {
-            n++;
-            if (crveni.pop().charAt(1) == '+')
-                izlez += "R- ";
-            else
-                izlez += "R+ ";
-        }
-
-        while (!zeleni.isEmpty()) {
-            n++;
-            if (zeleni.pop().charAt(1) == '+')
-                izlez += "G- ";
-            else
-                izlez += "G+ ";
-        }
-
-        while (!sini.isEmpty()) {
-            n++;
-            if (sini.pop().charAt(1) == '+')
-                izlez += "B- ";
-            else
-                izlez += "B+ ";
-        }
-
-        System.out.println(n);
-        return izlez;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String line = sc.nextLine(); // чита цел ред
-        String[] vlez = line.split(" "); // ги дели топчињата
-
-        System.out.println(ponistiTopcinja(vlez));
-    }
+import java.util.*;
+public class TopcinjaStack {
+public static void main(String [] args) {
+	Scanner input=new Scanner(System.in);
+	String topcinja=input.nextLine();//R+ G- G+ G+ R+ B- B+ R- G+ R- B- B+ B+ R+
+	ArrayStack<String>crveni=new ArrayStack<String>(100);
+	ArrayStack<String>zeleni=new ArrayStack<String>(100);
+	ArrayStack<String>sini=new ArrayStack<String>(100);
+	StringBuilder izlez=new StringBuilder();
+	String [] t=topcinja.split(" ");//"R+" "G-" "G+" G+ R+ B- B+ R- G+ R- B- B+ B+ R+
+	int n=0;
+	for(String i:t) {
+		 // ПРОВЕРКА: дали стрингот има барем 2 карактери (боја и знак)
+//        if (i.length() < 2) {
+//            // Игнорирај ако е невалиден внес
+//            continue;
+//        }
+		char boja=i.charAt(0);
+		char znak=i.charAt(1);
+		ArrayStack<String>stack=null;
+	
+	if(boja=='R') {
+		stack=crveni;
+	}
+	else if(boja=='G') {
+	stack=zeleni;
+	
 }
+	else if (boja=='B') {
+		stack=sini;
+	}
+	if(stack!=null) {
+		if(!stack.isEmpty()&&stack.peek().charAt(1)!=znak) {
+			stack.pop();
+		}
+		else {
+			stack.push(i);
+		}
+	}
+}
+	   while (!crveni.isEmpty()) {
+           n++;
+           izlez.append(crveni.pop().charAt(1) == '+' ? "R- " : "R+ ");
+       }
+
+       while (!zeleni.isEmpty()) {
+           n++;
+           izlez.append(zeleni.pop().charAt(1) == '+' ? "G- " : "G+ ");
+       }
+
+       while (!sini.isEmpty()) {
+           n++;
+           izlez.append(sini.pop().charAt(1) == '+' ? "B- " : "B+ ");
+       }
+
+       // Прво бројот на преостанати топчиња
+       System.out.println(n);
+       System.out.println(izlez);
+   }}
+	
+
